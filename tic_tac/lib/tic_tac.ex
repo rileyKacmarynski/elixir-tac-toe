13a44a48b4618fad9b98c096e2250559b7381580
Riley Kacmarynski <rkacmarynski@omnitech-inc.com>
2020-02-25 14:01:10 -0600

    Initialize repo with mix project.

---
+++ tic_tac/lib/tic_tac.ex
@@ -0,0 +1,18 @@
+defmodule TicTac do
+  @moduledoc """
+  Documentation for TicTac.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TicTac.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ tic_tac/lib/tic_tac/application.ex
@@ -0,0 +1,18 @@
+defmodule TicTac.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      {DynamicSupervisor, name: TicTac}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: TicTac.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
