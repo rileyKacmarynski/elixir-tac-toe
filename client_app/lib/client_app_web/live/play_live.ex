8ea0f4702a02be12bcf834ef32d2493ab369559c
rileykacmarynski <riley.kacmarynski@gmail.com>
2020-03-08 18:49:35 -0500

    Created tic tac toe board.

--- client_app/lib/client_app_web/live/play_live.ex
+++ client_app/lib/client_app_web/live/play_live.ex
@@ -4,36 +4,55 @@ defmodule ClientAppWeb.PlayLive do
   alias ClientAppWeb.Router.Helpers, as: Routes
   alias ClientAppWeb.Presence
 
-
-  @topic "play:"
-
   def mount(%{"game" => game_id}, session, socket) do
-    {:ok,
-      socket
-      |> assign(:current_user, Map.fetch!(session, "current_user"))
-      |> assign(game_id: game_id)
-      |> setup_presence()}
+    IO.inspect(socket)
+
+    socket = socket
+    |> assign(:current_user, Map.fetch!(session, "current_user"))
+    |> assign(game_id: game_id, users: [])
+    |> setup_presence()
+
+    # we are going to have to check to see if the game with that id has been started
+    # if it has we can get the game, if it hasn't we'll start it and get the game.
+    # This will ensure only one user start the game
+
+    {:ok, socket }
   end
 
   def render(assigns) do
-    IO.inspect(assigns)
     Phoenix.View.render(ClientAppWeb.PageView, "play.html", assigns)
   end
 
   defp setup_presence(
     socket = %{ assigns: %{ current_user: current_user, game_id: game_id }}
   ) do
-    topic = @topic <> to_string(game_id)
+    topic = "play:" <> to_string(game_id)
     ClientAppWeb.Endpoint.subscribe(topic)
 
     Presence.track(
       self(),
-      @topic,
+      topic,
       current_user,
       %{}
     )
 
-    socket
+    assign(socket, topic: topic)
   end
 
+  def handle_info(
+    %{event: "presence_diff", payload: %{joins: joins, leaves: leaves}},
+    %{assigns: %{topic: topic}} = socket
+  ) do
+    users =
+      Presence.list(topic)
+      |> Enum.map(fn {username, _data} -> username end)
+
+      # if both players have joined, start the game.
+
+      # IO.puts("diff called for user #{socket.assigns.current_user}")
+      # IO.inspect(users)
+      # IO.inspect(joins)
+
+      {:noreply, assign(socket, users: users )}
+  end
 end
